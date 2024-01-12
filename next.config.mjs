32fa7c8d990584b83bf43868efe32ab917ed137a
Todd Geist <todd@geistinteractive.com>
2024-01-12 14:56:40 -0800

    Update next.config.mjs with new redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,29 @@
 /** @type {import('next').NextConfig} */
 import nextra from "nextra";
 const nextConfig = {
-	reactStrictMode: true,
-	transpilePackages: ["ui", "common"],
-	async redirects() {
-		return [];
-	},
-	rewrites: async () => {
-		return [
-			{
-				source: "/apidoc",
-				destination: "/apidoc/index.html",
-			},
-		];
-	},
+  reactStrictMode: true,
+  transpilePackages: ["ui", "common"],
+  async redirects() {
+    return [];
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: "/apidoc",
+        destination: "/apidoc/index.html",
+      },
+      {
+        source: "/webhooks",
+        destination: "/guides/webhook",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withNextra = nextra({
-	theme: "nextra-theme-docs",
-	themeConfig: "./theme.config.tsx",
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
 });
 
 export default withNextra(nextConfig);
